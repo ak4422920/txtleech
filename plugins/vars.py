c0602273a2ce18baecb6bbd31a649d1195a887c1
ak4422920 <ak4422920@gmail.com>
2025-01-13 17:54:04 +0530

    Update vars.py

--- plugins/vars.py
+++ plugins/vars.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
+API_ID    = os.environ.get("API_ID", "29171167")
+API_HASH  = os.environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 WEBHOOK = True  # Don't change this
